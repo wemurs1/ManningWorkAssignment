2646ed9707cdddd6c43a390d8efac49911716461
Stuart Murray <stuart.murray.dev@outlook.com>
2022-10-07 14:21:10 +0100

    milestone 2 starter

---
+++ Milestone2/find_flows/Window1.xaml.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+using Microsoft.Win32;
+
+namespace find_flows
+{
+    /// <summary>
+    /// Interaction logic for Window1.xaml
+    /// </summary>
+    public partial class Window1 : Window
+    {
+        public Window1()
+        {
+            InitializeComponent();
+        }
+
+        private Network MyNetwork = new Network();
+
+        private void OpenCommand_CanExecute(object sender, CanExecuteRoutedEventArgs e)
+        {
+            e.CanExecute = true;
+        }
+
+        private void OpenCommand_Executed(object sender, ExecutedRoutedEventArgs e)
+        {
+            try
+            {
+                OpenFileDialog dialog = new OpenFileDialog();
+                dialog.DefaultExt = ".net";
+                dialog.Filter = "Network Files|*.net|All Files|*.*";
+
+                // Display the dialog.
+                bool? result = dialog.ShowDialog();
+                if (result == true)
+                {
+                    // Open the network.
+                    MyNetwork = new Network(dialog.FileName);
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+                MyNetwork = new Network();
+            }
+
+            // Display the network.
+            DrawNetwork();
+        }
+
+        private void DrawNetwork()
+        {
+            // Remove any previous drawing.
+            mainCanvas.Children.Clear();
+
+            // Make the network draw itself.
+            MyNetwork.Draw(mainCanvas);
+        }
+
+        private void ExitCommand_Executed(object sender, RoutedEventArgs e)
+        {
+            Close();
+        }
+
+        private void makeTestNetworks_Click(object sender, RoutedEventArgs e)
+        {
+            BuildGridNetwork("3x3_grid.net", 300, 300, 3, 3);
+            BuildGridNetwork("4x4_grid.net", 300, 300, 4, 4);
+            BuildGridNetwork("5x8_grid.net", 600, 400, 5, 8);
+            BuildGridNetwork("6x10_grid.net", 600, 400, 6, 10);
+            BuildGridNetwork("10x15_grid.net", 600, 400, 10, 15);
+            BuildGridNetwork("20x30_grid.net", 600, 400, 20, 30);
+            MessageBox.Show("Done");
+        }
+
+        // Set the network's shortest path algorithm.
+        private void algorithmComboBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            ComboBoxItem? item = algorithmComboBox.SelectedItem as ComboBoxItem;
+            if (item!.Content.ToString() == "Label Setting")
+                MyNetwork.AlgorithmType = Network.AlgorithmTypes.LabelSetting;
+            else if (item.Content.ToString() == "Label Correcting")
+                MyNetwork.AlgorithmType = Network.AlgorithmTypes.LabelCorrecting;
+        }
+    }
+}
---
+++ Milestone2/find_flows/Window_TestNetworks.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using System.Windows;
+
+namespace find_flows
+{
+    public partial class Window1
+    {
+        // Used to generate random link costs.
+        private Random Rand = new Random();
+
+        // Create a randomized grid network,
+        // save it into a file, and return it.
+        private Network BuildGridNetwork(string filename,
+            double width, double height, int numRows, int numCols)
+        {
+            const double MARGIN = 20;
+
+            double colWid = (width - 2 * MARGIN) / (numCols - 1);
+            double rowHgt = (height - 2 * MARGIN) / (numRows - 1);
+
+            // Make the network.
+            Network network = new Network();
+
+            // Make the nodes.
+            Node[,] nodes = new Node[numRows, numCols];
+            int index = 1;
+            for (int r = 0; r < numRows; r++)
+            {
+                for (int c = 0; c < numCols; c++)
+                {
+                    double x = MARGIN + c * colWid;
+                    double y = MARGIN + r * rowHgt;
+                    nodes[r, c] = new Node(network, new Point(x, y), index.ToString());
+                    index++;
+                }
+            }
+
+            // Make the horizontal links.
+            // This assumes that each link has a reversed
+            // link with a differently randomized cost.
+            for (int r = 0; r < numRows; r++)
+            {
+                for (int c = 0; c < numCols - 1; c++)
+                {
+                    MakeRandomizedLink(network, nodes[r, c], nodes[r, c + 1]);
+                }
+            }
+
+            // Make the horizontal links.
+            for (int r = 0; r < numRows - 1; r++)
+            {
+                for (int c = 0; c < numCols; c++)
+                {
+                    MakeRandomizedLink(network, nodes[r, c], nodes[r + 1, c]);
+                }
+            }
+
+            // Save the network into the file.
+            network.SaveIntoFile(filename);
+            return network;
+        }
+
+        // Make links between the nodes with lengths equal to the distance
+        // between them times a random amount between 1.0 and 1.2.
+        private void MakeRandomizedLink(Network network,
+            Node node1, Node node2)
+        {
+            double dist = Distance(node1.Center, node2.Center);
+
+            double cost12 = Math.Round(dist * Rand.Next(1.0, 1.2));
+            Link link12 = new Link(network, node1, node2, cost12);
+
+            double cost21 = Math.Round(dist * Rand.Next(1.0, 1.2));
+            Link link21 = new Link(network, node2, node1, cost21);
+        }
+
+        // Return the distance between two points.
+        private double Distance(Point point1, Point point2)
+        {
+            double dx = point1.X - point2.X;
+            double dy = point1.Y - point2.Y;
+            return Math.Sqrt(dx * dx + dy * dy);
+        }
+    }
+}
